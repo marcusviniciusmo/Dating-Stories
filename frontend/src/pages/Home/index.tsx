944c643828f56cf90947622e0ef12e8c396659f8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-09 20:11:08 +0100

    Navigation through pages created

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,3 +1,11 @@
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../routes/routes';
+
 export function Home() {
-  return <h1>HOME PAGE</h1>;
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <NavLink to={routes.stories}>Stories</NavLink>
+    </>
+  );
 }
--- frontend/src/pages/Stories/index.tsx
+++ frontend/src/pages/Stories/index.tsx
@@ -1,3 +1,11 @@
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../routes/routes';
+
 export function Stories() {
-  return <h1>STORIES PAGE</h1>;
+  return (
+    <>
+      <h1>STORIES PAGE</h1>
+      <NavLink to={routes.home}>Back</NavLink>
+    </>
+  );
 }
