2bb7bf9f5cba3a843ff6b916202ecffb6d95fd16
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-09 19:59:43 +0100

    NotFound page created

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Stories } from '../pages/Stories';
+import { NotFound } from '../pages/NotFound';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/stories" element={<Stories />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
