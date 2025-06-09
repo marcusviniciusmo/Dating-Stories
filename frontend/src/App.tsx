6e54cc17dd9445a28a4443ef414ee8ea234cf16d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-09 17:48:45 +0100

    Global style definition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
-  return <h1>Dating Stories</h1>;
+  return (
+    <>
+      <h1>Dating Stories</h1>
+      <GlobalStyle />
+    </>
+  );
 }
